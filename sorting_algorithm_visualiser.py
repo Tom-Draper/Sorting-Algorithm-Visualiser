8bd3f809a575e75dafa6fb74e4ff0dd868a3fcf1
Tom Draper <tomjdraper1@gmail.com>
2019-08-16 13:54:15 +0100

    Add sorting and display each pass

--- sorting_algorithm_visualiser.py
+++ sorting_algorithm_visualiser.py
@@ -34,6 +34,47 @@ def setup(start_x, start_y, add_x_const, add_y_const, list):
         pen.forward(add_y_const)
     turtle.update()
 
+def showPass(start_x, start_y, add_x_const, add_y_const, pass_no, my_list, previous_list):
+    pen = turtle.Turtle()
+    pen.hideturtle()
+    pen.speed(0)
+    pen.pensize(3)
+    pen.right(90)
+    turtle.tracer(False)
+
+    pen.color("yellow")
+    for item in previous_list:
+        x = start_x
+        y = start_y
+        previous_index = previous_list.index(item)
+        new_index = 0
+
+        for new_item in my_list:
+            if item == new_item:
+                new_index = my_list.index(new_item)
+                x += add_x_const * (previous_index + 1)
+                y -= add_y_const * pass_no
+                pen.up()
+                pen.goto(x, y)
+                pen.down()
+                x += add_x_const * (new_index - previous_index) 
+                y -= add_y_const
+                pen.goto(x, y)
+                break
+        turtle.update()
+
+def bubbleSort(start_x, start_y, add_x_const, add_y_const, my_list):
+    pass_no = 0
+    for i in range(len(my_list) -1, 0, -1):
+        for j in range(i):
+            pass_no += 1
+            previous_list = my_list[:]
+            if my_list[j] > my_list[j + 1]:
+                temp = my_list[j]
+                my_list[j] = my_list[j + 1]
+                my_list[j + 1] = temp
+            showPass(start_x, start_y, add_x_const, add_y_const, pass_no, my_list, previous_list)
+            
     turtle.done()
 
 def visualiser(my_list):
@@ -44,6 +85,6 @@ def visualiser(my_list):
     add_y_const = 50
 
     setup(start_x, start_y, add_x_const, add_y_const, my_list)
-
+    bubbleSort(start_x, start_y, add_x_const, add_y_const, my_list)
 
 visualiser([5, 4, 3, 2, 1])
